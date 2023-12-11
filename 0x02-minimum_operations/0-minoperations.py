129f078c41fec1c2d5d0424470c42cf2d8377515
martinmulama <martin.mulama@yahoo.com>
2023-12-11 18:00:17 +0000

    added 0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+'''
+Given a number n, write a method that calculates
+the fewest number of operations needed to result in
+exactly n H characters in the file.
+'''
+
+
+def minOperations(n):
+    '''Returns min operations to get n Hs.'''
+
+    result = 0
+    index = 2
+    if n < 2:
+        return 0
+    while (index < n + 1):
+        while n % index == 0:
+            result += index
+            n = n / index
+        index += 1
+    return result
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,3 @@
+Minimum Operations
+File: 4-minimum_operations.py
+Description: Solves the "2 keys keyboard" problem, calculating the fewest number of operations needed to achieve a certain number of characters.
